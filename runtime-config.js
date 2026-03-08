137e59d55940e0ec385e67a490947d6dd2113415
Nana Blay Eku-Kintoh <123515399+Blaygh@users.noreply.github.com>
2026-03-08 12:10:36 -0400

    Move Supabase runtime config to file and gate debug responses

---
+++ runtime-config.js
@@ -0,0 +1,6 @@
+window.MODARIO_SUPABASE = {
+  url: 'https://bcaajafisvrttvinjfad.supabase.co',
+  anonKey: 'sb_publishable_EzlbWXkh0hMbajPO63X6Vg_wvqTXvLh',
+  functionName: 'join-waitlist',
+  debugResponses: false,
+};
